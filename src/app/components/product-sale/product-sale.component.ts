79395ce5c500a8b42a37fb27b9a984fa7bdab8a8
Denix7 <dennis.brian.pv@gmail.com>
2020-01-28 16:34:27 -0400

    Creacion de producto sin subir imagen todavia

--- src/app/components/product-sale/product-sale.component.ts
+++ src/app/components/product-sale/product-sale.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { RestApiService } from '../../services/rest-api.service';
+import { DataService } from '../../services/data.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-sale',
@@ -7,9 +10,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductSaleComponent implements OnInit {
 
-  constructor() { }
+  private name:string = '';
+  private price:string = '';
+  private description:string = '';
+  private category:string;
+
+  constructor(private router: Router, private rest: RestApiService, private data:DataService) { }
 
   ngOnInit() {
   }
 
+  async createProduct()
+  {
+    console.warn('product sale');
+    try{
+      const data = await this.rest.post('http://localhost:3030/api/seller/products', 
+    {
+      name : this.name,
+      price : this.price,
+      description: this.description
+    });
+
+    if(data['success'])
+    {
+      this.data.success('Product created successful');
+      this.router.navigate(['profile/products/my-products'])
+    }
+    else
+    {
+      this.data.error(data['message']);
+    }
+    }
+    catch(error)
+    {
+      this.data.error(error['message']);
+    }
+  }
 }
