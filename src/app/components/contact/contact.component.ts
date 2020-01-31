abe11be44ed8c6fd3c2cec8f5b6601901121858f
Denix7 <dennis.brian.pv@gmail.com>
2020-01-31 19:03:25 -0400

    Envio de email a travez de la ventana de contacto

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { EmailService } from '../../services/email.service';
+import swal from 'SweetAlert';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
-export class ContactComponent implements OnInit {
 
-  constructor() { }
+export class ContactComponent implements OnInit {
+  
+  formulario =   {
+    nombre: '',
+    apellido: '',
+    email: '',
+    asunto: '',
+    mensaje: ''
+  }
+
+  constructor(private _emailService: EmailService) { }
 
   ngOnInit() {
   }
 
+  contactForm(form)
+  {
+    this._emailService.sendMessage(form).subscribe(()=>{
+      swal("Formulario de contacto",
+            "Mensaje enviado correctamente",
+            'success');
+
+      this.cleanForm();
+    });
+  }
+  
+  cleanForm()
+  {
+    this.formulario.nombre='';
+    this.formulario.apellido='';
+    this.formulario.email='';
+    this.formulario.asunto='';
+    this.formulario.mensaje='';
+
+  }
 }
---
+++ src/app/services/email.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmailService {
+
+  constructor(private _http:HttpClient) {}
+
+  sendMessage(body)
+  {
+    return this._http.post('http://localhost:3030/api/email', body);
+  }
+}
